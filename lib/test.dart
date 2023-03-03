a28f73429aa897a84101ab43d48d4ac4b07bf8af
ziad <ziadelsaadany73@gmail.com>
2023-03-03 23:08:40 +0200

    add test class

---
+++ lib/test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TestScreen extends StatelessWidget {
+  const TestScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
